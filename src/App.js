17879e11137949cbd0e93b531d417bc98f208d2b
Liam Morley <liam@carpeliam.com>
2019-03-31 11:10:11 -0400

    Add chooser to choose between municipal and federal levels.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,26 @@ import GovernmentFixer from './GovernmentFixer';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { size: 'municipal' };
+  }
+  chooseLevel = (event) => {
+    this.setState({ size: event.target.value });
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <GovernmentFixer size="municipal" />
+          <label>
+            <input type="radio" name="size" checked={this.state.size == 'municipal'} value="municipal" onChange={this.chooseLevel} />
+            Municipal
+          </label>
+          <label>
+            <input type="radio" name="size" checked={this.state.size == 'federal'} value="federal" onChange={this.chooseLevel} />
+            Federal
+          </label>
+          <GovernmentFixer size={this.state.size} />
         </header>
       </div>
     );
